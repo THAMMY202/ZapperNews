9059eff978f8d42dc832856bd7d2fb37e7f22e83
Thamsanqa Shabalala <thammy202@gmail.com>
2022-08-15 20:13:53 +0200

    Added Config file

--- app/src/main/java/com/thamsanqa/zappernews/MainActivity.java
+++ app/src/main/java/com/thamsanqa/zappernews/MainActivity.java
@@ -9,6 +9,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.thamsanqa.zappernews.api.ApiClient;
 import com.thamsanqa.zappernews.pojo.News;
+import com.thamsanqa.zappernews.util.Config;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -18,26 +19,23 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Trailing slash is needed
-    public static final String BASE_URL = "http://api.mediastack.com/v1/";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Gson gson = new GsonBuilder()
-                .setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+                .setDateFormat(Config.DATE_FORMAT)
                 .create();
 
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(BASE_URL)
+                .baseUrl(Config.BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create(gson))
                 .build();
 
         ApiClient apiService = retrofit.create(ApiClient.class);
 
-        Call<News> call = apiService.getNews("34aad064e45ddcd0ff6d0341699d6b1b");
+        Call<News> call = apiService.getNews(Config.ACCESS_KEY);
         call.enqueue(new Callback<News>() {
             @Override
             public void onResponse(Call<News> call, Response<News> response) {
@@ -45,12 +43,10 @@ public class MainActivity extends AppCompatActivity {
                 if (response != null && response.isSuccessful()) {
                     News news = response.body();
                 }
-
             }
 
             @Override
             public void onFailure(Call<News> call, Throwable t) {
-                // Log error here since request failed
                 showMsg(t.toString());
             }
         });
---
+++ app/src/main/java/com/thamsanqa/zappernews/util/Config.java
@@ -0,0 +1,7 @@
+package com.thamsanqa.zappernews.util;
+
+public class Config {
+    public static final String ACCESS_KEY = "34aad064e45ddcd0ff6d0341699d6b1b";
+    public static final String BASE_URL = "http://api.mediastack.com/v1/";
+    public static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ssZ";
+}
