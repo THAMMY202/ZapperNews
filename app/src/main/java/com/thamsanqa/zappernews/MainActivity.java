0dacc3373445374bb54ad2955e955b6b027ace3b
Thamsanqa Shabalala <thammy202@gmail.com>
2022-08-15 23:18:38 +0200

    Displaying news to the screen

--- app/src/main/java/com/thamsanqa/zappernews/MainActivity.java
+++ app/src/main/java/com/thamsanqa/zappernews/MainActivity.java
@@ -4,12 +4,18 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.thamsanqa.zappernews.adapter.NewsAdapter;
 import com.thamsanqa.zappernews.api.ApiClient;
 import com.thamsanqa.zappernews.pojo.News;
 import com.thamsanqa.zappernews.util.Config;
+
+import java.util.ArrayList;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -17,12 +23,22 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+/**
+ * This activity is the app entry point and it fetch data from the api
+ */
+
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<News> arrayListNews = new ArrayList<News>();
+    private RecyclerView recyclerView;
+    private NewsAdapter newsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerviewStories);
 
         Gson gson = new GsonBuilder()
                 .setDateFormat(Config.DATE_FORMAT)
@@ -42,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
 
                 if (response != null && response.isSuccessful()) {
                     News news = response.body();
+                    setData(news);
                 }
             }
 
@@ -50,6 +67,14 @@ public class MainActivity extends AppCompatActivity {
                 showMsg(t.toString());
             }
         });
+    }
+
+    private void setData(News news){
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(MainActivity.this, 1, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(gridLayoutManager);
+        newsAdapter = new NewsAdapter(MainActivity.this, news);
+        recyclerView.setAdapter(newsAdapter);
+
     }
 
     private void showMsg(String message) {
